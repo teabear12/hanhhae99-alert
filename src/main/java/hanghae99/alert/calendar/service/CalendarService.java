0c749e833102025df0fece84c3bc6bcdb8307066
suhjaesuk <suhjaesuk@gmail.com>
2022-12-18 00:51:15 +0900

    Feat : 일정 전체 조회 추가

--- src/main/java/hanghae99/alert/calendar/service/CalendarService.java
+++ src/main/java/hanghae99/alert/calendar/service/CalendarService.java
@@ -1,8 +1,10 @@
 package hanghae99.alert.calendar.service;
 
+import hanghae99.alert.calendar.dto.CalendarListInfoResponseDto;
 import hanghae99.alert.calendar.dto.CalendarSaveRequestDto;
 
 public interface CalendarService {
     void createCalendar(CalendarSaveRequestDto calendarSaveRequestDto, String username);
 
+    CalendarListInfoResponseDto getCalendarListInfo(String username);
 }
--- src/main/java/hanghae99/alert/calendar/service/CalendarServiceImpl.java
+++ src/main/java/hanghae99/alert/calendar/service/CalendarServiceImpl.java
@@ -1,8 +1,11 @@
 package hanghae99.alert.calendar.service;
 
+import hanghae99.alert.calendar.dto.CalendarListInfo;
+import hanghae99.alert.calendar.dto.CalendarListInfoResponseDto;
 import hanghae99.alert.calendar.dto.CalendarSaveRequestDto;
 import hanghae99.alert.calendar.entity.Calendar;
 import hanghae99.alert.calendar.repository.CalendarRepository;
+import hanghae99.alert.member.entity.Member;
 import hanghae99.alert.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -24,13 +27,16 @@ public class CalendarServiceImpl implements CalendarService {
     }
 
 
-
-
-
-
-
-
-
+    @Override
+    public CalendarListInfoResponseDto getCalendarListInfo(String username) {
+        CalendarListInfoResponseDto calendarList = new CalendarListInfoResponseDto();
+        /* 정렬 필요할 시 조건 추가 */
+        Member member = checkMember(username);
+        for(Calendar calendar : member.getCalendarList()){
+            calendarList.addCalendar(new CalendarListInfo(calendar));
+        }
+        return calendarList;
+    }
 
     private Calendar checkCalendar(Long calendarId){
         return calendarRepository.findById(calendarId).orElseThrow(()-> new IllegalArgumentException("존재하지 않은 게시물입니다."));
