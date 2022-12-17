3148e3d68887fdfe885e57229a4f78f6c3f594f9
SeDevel <ohhojin25@naver.com>
2022-12-17 18:56:39 +0900

    Squashed commit of the following:
    commit 91aa723ab228064af04dd02e15f86bf39ea49322
    Merge: 94b322f b1208c4
    Author: SeDevel <31820402+1Tsw0rd@users.noreply.github.com>
    Date:   Sat Dec 17 18:55:31 2022 +0900
    
        Merge pull request #7 from teabear12/feature/#login-basic
    
        feat: login basic
        코드 확인
    
    commit b1208c49d6474e197a2f6820e697bde3c54eefc2
    Merge: 44327b5 94b322f
    Author: SeDevel <31820402+1Tsw0rd@users.noreply.github.com>
    Date:   Sat Dec 17 18:55:12 2022 +0900
    
        Merge branch 'develop' into feature/#login-basic
    
    commit 94b322f76ab880d3f85ed8bd5669653de83b74a5
    Merge: e1ce54a c02a605
    Author: SeDevel <31820402+1Tsw0rd@users.noreply.github.com>
    Date:   Sat Dec 17 18:52:08 2022 +0900
    
        Merge pull request #6 from teabear12/feature/#signup-basic
    
        회원가입 코드 확인 완료
    
    commit 44327b5e4ed1dad10cabd4635851f8a73788a8e2
    Merge: f9fead1 e1ce54a
    Author: S_0ww <114342129+seok6086@users.noreply.github.com>
    Date:   Sat Dec 17 17:15:39 2022 +0900
    
        Merge branch 'develop' into feature/#login-basic
    
    commit f9fead15767c8197aad18cbe6715c83af5d47caf
    Author: 황보석 <zheoajfl@gmail.com>
    Date:   Sat Dec 17 17:13:12 2022 +0900
    
        feat: 기초적인 로그인 기능 구현
    
    commit c02a605abf6aac1fbce08ddc194e17bbd4c5f4d1
    Merge: 498da97 e1ce54a
    Author: S_0ww <114342129+seok6086@users.noreply.github.com>
    Date:   Sat Dec 17 16:30:17 2022 +0900
    
        Merge branch 'develop' into feature/#signup-basic
    
    commit 498da9791db83165e4d256f848203aa0bf2fb9ba
    Author: 황보석 <zheoajfl@gmail.com>
    Date:   Sat Dec 17 16:25:25 2022 +0900
    
        feat: 인증인가 '없는' 회원가입 기능 / 테스트 구동시 build.gradle 멤버디테일부분 / MemberDetail 서비스랑 mpl 전체 주석처리해야함
    
    commit e1ce54a66607fc0d8f9cd546eb2664566754f3b7
    Merge: 5359eb2 1416e37
    Author: teabear12 <118160987+teabear12@users.noreply.github.com>
    Date:   Sat Dec 17 15:34:09 2022 +0900
    
        Merge pull request #5 from teabear12/feautre/featException
    
        feat DataResponse 추가
    
    commit 1416e37d03a54a4c5459a13841c37ed02f3113f3
    Author: SeDevel <ohhojin25@naver.com>
    Date:   Sat Dec 17 15:31:05 2022 +0900
    
        feat DataResponse 추가

--- src/main/java/hanghae99/alert/member/repository/MemberRepository.java
+++ src/main/java/hanghae99/alert/member/repository/MemberRepository.java
@@ -3,5 +3,8 @@ package hanghae99.alert.member.repository;
 import hanghae99.alert.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Object> findByUsername(String username);
 }
