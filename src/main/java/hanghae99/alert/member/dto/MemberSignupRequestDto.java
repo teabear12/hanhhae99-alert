498da9791db83165e4d256f848203aa0bf2fb9ba
황보석 <zheoajfl@gmail.com>
2022-12-17 16:25:25 +0900

    feat: 인증인가 '없는' 회원가입 기능 / 테스트 구동시 build.gradle 멤버디테일부분 / MemberDetail 서비스랑 mpl 전체 주석처리해야함

--- src/main/java/hanghae99/alert/member/dto/MemberSignupRequestDto.java
+++ src/main/java/hanghae99/alert/member/dto/MemberSignupRequestDto.java
@@ -1,4 +1,20 @@
 package hanghae99.alert.member.dto;
 
+import hanghae99.alert.member.entity.Member;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
 public class MemberSignupRequestDto {
+
+    private String username;
+    private String nickname;
+    private String password;
+
+    public Member toMember(MemberSignupRequestDto memberSignupRequestDto) {
+        return new Member(memberSignupRequestDto.getUsername(),
+                memberSignupRequestDto.getNickname(),
+                memberSignupRequestDto.getPassword() );
+    }
 }
--- src/main/java/hanghae99/alert/member/service/MemberServiceImpl.java
+++ src/main/java/hanghae99/alert/member/service/MemberServiceImpl.java
@@ -1,5 +1,9 @@
 package hanghae99.alert.member.service;
 
+import hanghae99.alert.global.exception.CustomErrorCodeEnum;
+import hanghae99.alert.global.exception.CustomException;
+import hanghae99.alert.member.dto.MemberSignupRequestDto;
+import hanghae99.alert.member.entity.Member;
 import hanghae99.alert.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,4 +14,9 @@ public class MemberServiceImpl implements MemberService {
 
     private final MemberRepository memberRepository;
 
+    @Override
+    public void signup(MemberSignupRequestDto memberSignupRequestDto) {
+        Member member = memberSignupRequestDto.toMember(memberSignupRequestDto);
+        memberRepository.save(member);
+    }
 }
