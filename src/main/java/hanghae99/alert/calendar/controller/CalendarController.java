49909bcf9c8da839de64d701d8eb2be10f798daa
suhjaesuk <suhjaesuk@gmail.com>
2022-12-17 15:36:37 +0900

    Comment : 주석 추가

--- src/main/java/hanghae99/alert/calendar/controller/CalendarController.java
+++ src/main/java/hanghae99/alert/calendar/controller/CalendarController.java
@@ -2,8 +2,7 @@ package hanghae99.alert.calendar.controller;
 
 import hanghae99.alert.calendar.service.CalendarService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -11,6 +10,26 @@ import org.springframework.web.bind.annotation.RestController;
 public class CalendarController {
 
     private final CalendarService calendarService;
+
+    /* 일정 등록 */
+    @PostMapping
+
+
+
+    /* 일정 전체 조회 */
+
+
+
+    /* 일정 상세 조회 */
+
+
+
+    /* 일정 수정 */
+
+
+
+    /* 일정 삭제 */
+
 
 
 
