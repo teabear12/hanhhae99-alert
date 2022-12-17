b44f558d8d1d274933859f2ab80c0a05ee9d4696
suhjaesuk <suhjaesuk@gmail.com>
2022-12-17 17:27:30 +0900

    Feat : CalendarController 모든 기능 추가, 일정 저장 기능 구현

--- src/main/java/hanghae99/alert/calendar/service/CalendarServiceImpl.java
+++ src/main/java/hanghae99/alert/calendar/service/CalendarServiceImpl.java
@@ -1,15 +1,44 @@
 package hanghae99.alert.calendar.service;
 
+import hanghae99.alert.calendar.dto.CalendarSaveRequestDto;
+import hanghae99.alert.calendar.entity.Calendar;
 import hanghae99.alert.calendar.repository.CalendarRepository;
+import hanghae99.alert.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class CalendarServiceImpl implements CalendarService {
 
     private final CalendarRepository calendarRepository;
+    private final MemberRepository memberRepository;
+
     @Override
-    public void calendarSave() {
+    @Transactional
+    public void createCalendar(CalendarSaveRequestDto calendarSaveRequestDto, String username) {
+        checkMember(username);
+        Calendar calendar = calendarSaveRequestDto.toEntity();
+        calendarRepository.save(calendar);
     }
+
+
+
+
+
+
+
+
+
+
+    private Calendar checkCalendar(Long calendarId){
+        return calendarRepository.findById(calendarId).orElseThrow(()-> new IllegalArgumentException("존재하지 않은 게시물입니다."));
+    }
+
+    private void checkMember(String username){
+        memberRepository.findByUsername(username).orElseThrow(()-> new IllegalArgumentException("로그인이 필요합니다."));
+    }
+
+
 }
--- src/main/java/hanghae99/alert/member/repository/MemberRepository.java
+++ src/main/java/hanghae99/alert/member/repository/MemberRepository.java
@@ -3,5 +3,9 @@ package hanghae99.alert.member.repository;
 import hanghae99.alert.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByUsername(String username);
 }
