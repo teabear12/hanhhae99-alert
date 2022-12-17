caed3743ad884ee9707bce356ffcb4a22cc36a77
suhjaesuk <suhjaesuk@gmail.com>
2022-12-17 16:08:00 +0900

    Feat : CalendarController 등록 추가

--- src/main/java/hanghae99/alert/calendar/controller/CalendarController.java
+++ src/main/java/hanghae99/alert/calendar/controller/CalendarController.java
@@ -1,8 +1,16 @@
 package hanghae99.alert.calendar.controller;
 
+import hanghae99.alert.calendar.dto.CalendarSaveRequestDto;
 import hanghae99.alert.calendar.service.CalendarService;
+import hanghae99.alert.global.response.Response;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static hanghae99.alert.global.response.ResponseMessage.CREATE_CALENDAR_SUCCESS_MSG;
 
 @RestController
 @RequiredArgsConstructor
@@ -10,9 +18,13 @@ import org.springframework.web.bind.annotation.*;
 public class CalendarController {
 
     private final CalendarService calendarService;
-
     /* 일정 등록 */
     @PostMapping
+    public Response createCalendar(@AuthenticationPrincipal UserDetailsImpl userDetails, @RequestBody CalendarSaveRequestDto calendarSaveRequestDto) {
+        String username = userDetails.getUsername();
+        calendarService.createCalendar(calendarSaveRequestDto);
+        return new Response(CREATE_CALENDAR_SUCCESS_MSG);
+    }
 
 
 
