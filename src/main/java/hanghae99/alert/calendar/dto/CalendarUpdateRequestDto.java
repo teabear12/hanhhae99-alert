bf75a2be3e8f6bc92ba805a0db0e096e30b448fd
JaeSuk <110963294+suhjaesuk@users.noreply.github.com>
2022-12-20 16:35:52 +0900

    Feat: : 스웨거 추가 (#28)

--- src/main/java/hanghae99/alert/calendar/dto/CalendarUpdateRequestDto.java
+++ src/main/java/hanghae99/alert/calendar/dto/CalendarUpdateRequestDto.java
@@ -1,9 +1,14 @@
 package hanghae99.alert.calendar.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
-
+@ApiModel(value = "일정 수정 정보")
 @Getter
 public class CalendarUpdateRequestDto {
+
+    @ApiModelProperty(value="일정 내용", dataType = "String", example="내용")
     private String content;
+    @ApiModelProperty(value="마감 시간", dataType = "Long", example="1671436584761")
     private String endTime;
 }
