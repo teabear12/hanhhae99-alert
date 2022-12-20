bb47b249b5d40f22c5191d6bbd05562928b28955
SeDevel <31820402+1Tsw0rd@users.noreply.github.com>
2022-12-20 16:40:22 +0900

    fix : CORS 및 시큐리티 설정 변경 (#30)

--- src/main/java/hanghae99/alert/global/security/WebSecurityConfig.java
+++ src/main/java/hanghae99/alert/global/security/WebSecurityConfig.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
@@ -47,8 +48,9 @@ public class WebSecurityConfig {
 
         // .authorizeRequests() : 요청에 대한 권한을 지정
         http.authorizeRequests()
-                .antMatchers("/auth/signup").permitAll()
-                .antMatchers("/auth/login").permitAll()
+                .antMatchers(HttpMethod.OPTIONS,"/auth/*").permitAll() //CORS preflight 예비통신 전용
+                .antMatchers(HttpMethod.POST, "/auth/signup").permitAll()
+                .antMatchers(HttpMethod.POST, "/auth/login").permitAll()
                 /* swagger */
                 .antMatchers("/v2/api-docs", "/swagger*/**").permitAll()
                 .antMatchers("/swagger-resources/configuration/ui").permitAll()
