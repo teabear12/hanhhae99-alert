81a4f3b55ca6c05b1b5d9d297912556f3fec3b7d
suhjaesuk <suhjaesuk@gmail.com>
2022-12-20 10:47:14 +0900

    Fix : 캘린더 전체 조회 데이터 값 추가

--- src/main/java/hanghae99/alert/calendar/dto/CalendarListInfo.java
+++ src/main/java/hanghae99/alert/calendar/dto/CalendarListInfo.java
@@ -3,15 +3,29 @@ package hanghae99.alert.calendar.dto;
 import hanghae99.alert.calendar.entity.Calendar;
 import lombok.Getter;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 @Getter
 public class CalendarListInfo {
     private Long calendarId;
     private String content;
-    private Long endTime;
+    private Long currentTimeMillis;
+    private Long endTimeMillis;
+    private String endTime;
+    private String startTime;
 
     public CalendarListInfo(Calendar calendar){
         this.calendarId = calendar.getId();
         this.content = calendar.getContent();
-        this.endTime = calendar.getEndTime();
+        this.currentTimeMillis = System.currentTimeMillis();
+        this.endTimeMillis = calendar.getEndTime();
+        this.startTime=changeMillisToDate(calendar.getStartTime());
+        this.endTime=changeMillisToDate(calendar.getEndTime());
+    }
+
+    private String changeMillisToDate(Long millis){
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yy/MM/dd hh시 mm분");
+        return dateFormat.format(new Date(millis));
     }
 }
