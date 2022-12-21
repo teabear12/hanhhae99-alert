047c70439739546e828fc88e488e69b6ef2b13dc
JaeSuk <110963294+suhjaesuk@users.noreply.github.com>
2022-12-21 18:02:08 +0900

    Feat : 타임존 아시아로 설정 (#34)

--- src/main/java/hanghae99/alert/AlertApplication.java
+++ src/main/java/hanghae99/alert/AlertApplication.java
@@ -4,9 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @EnableScheduling
 @SpringBootApplication
 public class AlertApplication {
+	@PostConstruct
+	public void setUp() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(AlertApplication.class, args);
 	}
